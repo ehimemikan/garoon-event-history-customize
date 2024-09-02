b81b92488c1f413d89917282b660982b663b2ed8
Yuya Kawabata <yuya-kawabata@cybozu.co.jp>
2024-09-02 05:03:53 +0000

    アウトラインをまとめた

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-console.log('hoge');
+JS APIで登録成功, 簡易登録成功, 変更成功, 簡易変更成功イベントをフックする(){
+    予定内容をdatastoreへ保存する 
+}
 
+JS APIで予定の表示イベントをフックする(){  
+    取得用のボタンをinsertTableRowで表示する
+}
 
+function ボタンを押した時の内容(){
+  datastoreの内容を呼び出し、JSONの差分を計算する
+  ボタンを消してinsertTableRowに差分を表示する
+}
