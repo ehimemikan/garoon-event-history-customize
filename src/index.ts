84363e3087835ce9b18b4ce3a8613abe07c57cee
Yasuharu.S <ehimemikan@users.noreply.github.com>
2024-09-02 09:13:35 +0000

    基本的なdatastoreと表示の動作を作成

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,6 @@ interface HistoryData {
   notes: string,
 }
 
-interface HTMLElementEvent<T extends HTMLElement> extends Event {
-  target: T;
-}
-
 garoon.events.on([
   "schedule.event.create.submit.success",
   "schedule.event.quick.create.submit.success",
@@ -33,9 +29,10 @@ function JSAPIで予定の表示イベントをフックする() {
   取得用のボタンをinsertTableRowで表示する();
 }
 
-function ボタンを押した時の内容(this: HTMLElementEvent<HTMLButtonElement>, event: MouseEvent): void {
+function ボタンを押した時の内容(event: MouseEvent): void {
+  const el = event.target as HTMLButtonElement;
   datastoreの内容を呼び出しJSONの差分を計算する();
-  ボタンを消してinsertTableRowに差分を表示する();
+  ボタンを消してinsertTableRowに差分を表示する(el);
 }
 
 function 予定内容をdatastoreへ保存する(event: ScheduleEvent): void {
@@ -66,8 +63,11 @@ function datastoreの内容を呼び出しJSONの差分を計算する() {
   // いったん生dataを出す
 }
 
-function ボタンを消してinsertTableRowに差分を表示する(element: HTMLElementEvent<HTMLButtonElement>, event: MouseEvent): any {
+function ボタンを消してinsertTableRowに差分を表示する(element: HTMLButtonElement): void {
   const histories = これまでの予定の内容をdatastoreから取得する();
+  const parent = element.parentElement as HTMLElement;
+  element.remove();
+  parent.textContent = JSON.stringify(histories, null, 2);
 }
 
 function ScheduleEventからHistoryDataを作る(event: ScheduleEvent): HistoryData {
